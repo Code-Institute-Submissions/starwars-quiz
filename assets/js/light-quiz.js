7918edbf10b18292bf4b60d16ed75461ce619744
lewejuice <hamiltonlewis@hotmail.com>
2020-06-15 12:36:40 +0000

    added further java script functionality

--- assets/js/light-quiz.js
+++ assets/js/light-quiz.js
@@ -17,21 +17,21 @@ const progress = document.getElementById("progress");
 
 let questions = [
     {
-        question: "Question 1?",
+        question: "What is the name of Princess Leia's home world?",
         imgSrc: "#",
-        optionA: "First option",
-        optionB: "Second option",
-        optionC: "Third option",
-        optionD: "Fourth option",
+        optionA: "Naboo",
+        optionB: "Tatooine",
+        optionC: "Alderaan",
+        optionD: "Kamino",
         correct: "C"
     },
     {
-        question: "Question 2?",
+        question: "What was the name of Emporer Palpatines master?",
         imgSrc: "#",
-        optionA: "First option",
-        optionB: "Second option",
-        optionC: "Third option",
-        optionD: "Fourth option",
+        optionA: "Darth Plagueis the wise",
+        optionB: "Darth Plaggmis the cunning",
+        optionC: "Darth Plagueis the cunning",
+        optionD: "Darth Plaggmis the wise",
         correct: "A"
     },
     {
@@ -109,7 +109,7 @@ let questions = [
 ];
 
 let lastQuestion = questions.length - 1;
-let currentquestion = 0;
+let currentQuestion = 0;
 
 function questionStructure() {
     let q = questions[currentQuestion];
@@ -125,4 +125,73 @@ function progressStructure() {
     for(let qIndex = 0; qIndex <= lastQuestion; qIndex ++) {
         progress.innerHTML += "<div class='progr' id=" + qIndex + "></div>";
     }
+};
+
+function correctAnswer() {
+    document.getElementById(currentquestion).style.backgroundColor = "blue";
+};
+
+function wrongAnswer() {
+    document.getElementById(currentquestion).style.backgroundColor = "red";
+};
+
+const questTimeLimit = 20;
+let count = 0;
+let countTimer = setInterval(counterStructure, 1000);
+let score = 0;
+
+function counterStructure() {
+    if (count <= questTimeLimit) {
+        counter.innerHTML = count;
+        count++;
+    } else {
+        count = 0;
+        wrongAnswer();
+        if(currentquestion < lastQuestion) {
+            currentquestion++;
+            questionStructure();
+        } else {
+            clearInterval(countTimer);
+            scoreStructure();
+        }
+    } 
+};
+
+function answerCheck(answer) {
+    if(questions[currentquestion].correct == answer) {
+        score++;
+        correctAnswer();
+    } else {
+        wrongAnswer();
+    }
+    if (currentQuestion < lastQuestion) {
+        count = 0;
+        currentQuestion++;
+        questionStructure();
+    } else {
+        clearInterval(countTimer);
+        scoreStructure();
+    }
+};
+
+start.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    start.style.display = "none";
+    counterStructure();
+    countTimer = setInterval(counterStructure, 1000);
+    questionStructure();
+    quiz.css.style.display = "block";
+};
+
+function scoreStructure() {
+    let scoreContainer = document.getElementById("score-container");
+    let scorePercentage = Math.round(100 * score / questions.length);
+    let img = (scorePercentage == 100) ? "assets/images/6.png":
+              (scorePercentage >= 85) ? "assets/images/5.png":
+              (scorePercentage >= 70) ? "assets/images/4.png":
+              (scorePercentage >= 50) ? "assets/images/3.png":
+              (scorePercentage >= 30) ? "assets/images/2.png": "assets/images/1.png";
+    scoreContainer.style.display = "block";
+    scoreContainer.innerHTML = "<img src=" + img + "><p>" + scorePercentage + "</p>";
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,14 @@ setTimeout(function() {
     $(".fade").fadeOut("fast");
 }, 350);
 
-$(".light-container").hover(function(){
-  $(this).css("background-color", "grey" + "color", "white");  
+$(".light-side").hover(function(){
   $(".blue-saber").css("visibility", "visible");
   }, function(){
-  $(this).css("background-color", "transparent" + "color", "blue");
   $(".blue-saber").css("visibility","hidden");
 });
 
-$(".dark-container").hover(function(){
+$(".dark-side").hover(function(){
   $(".red-saber").css("visibility", "visible");
-  $(this).css("background-color", "grey" + "color", "white");
   }, function(){
   $(".red-saber").css("visibility","hidden");
-  $(this).css("background-color", "transparent" + "color", "red");
 });
